b91fd56d33e0548a0ed96f3279df5bec5324c7f4
Brham Dev Mahato <brham1980@gmail.com>
2021-08-20 08:44:30 +0545

    basic javascript

---
+++ JavaScript Basics/42-template-literal/tutorial/app.js
@@ -0,0 +1,13 @@
+// Template Literals - ES6+
+// Backtick characters `` - above tab (left from one)
+// Interpolation ${} - insert expression(value)
+
+const fname = 'john';
+const age = 25;
+const sentence = "Hey it's " + fname + ' and he is ' + age + ' years old';
+
+const value = `Hey it's ${fname} and he is ${age} years old. And here is some simple math ${4 + 4}`;
+
+console.log(value);
+
+console.log(sentence);
